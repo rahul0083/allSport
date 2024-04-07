d97e337b458e31e4a47f9f2710fbb6cb166f4aa3
Rahul Singh <rahulme0083@gmail.com>
2024-04-07 22:04:51 +0530

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="pt-lg-10 pt-5 footer">
-      <div className="pb-0 container">
+      <div className="pb-0">
         <div className="row">
           <div className="col-lg-4 col-md-6 col-12 text-white col-md-6 col">
             <div className="mb-4">
@@ -12,13 +12,13 @@ function Footer() {
                 <p className="h5 fw-light lh-26 mb-5">2024</p>
                 <p className="h5 fw-light lh-26 mb-5">DOWNLOAD THE APP</p>
 
-                <a href="tel:+919703454549" className="text-white h4 fw-light">
-                  <img src="../../whatsAppIcon.svg" alt="" /> +91 9703454549
-                </a>
                 <div className="fs-4 mt-4 d-flex mt-6">
-                  <a href="#" className="me-2"><img src="../../facebookIcon.svg" alt="Facebook Icon" /></a>
-                  <a href="#" className="me-2"><img src="../../instagramIcon.svg" alt="Instagram Icon" /></a>
-                  <a href="#" className=""><img src="../../twitterIcon.svg" alt="Twitter Icon" /></a>
+                  <a href="#" className="me-2 text-white"><img src="../Vectorx.svg" alt="Facebook Icon" /></a>
+                  <a href="#" className="me-2"><img src="../Vectorfacebook.svg" alt="Instagram Icon" /></a>
+                  <a href="#" className=""><img src="../Groupinstagram.svg" alt="Twitter Icon" /></a>
+                  <a href="#" className=""><img src="../Vectorbird.svg" alt="Twitter Icon" /></a>
+                  <a href="#" className=""><img src="../Vectorlinkedin.svg" alt="Twitter Icon" /></a>
+
                 </div>
               </div>
             </div>
@@ -41,26 +41,27 @@ function Footer() {
           </div>
           <div className="col-lg-2 col-md-3 col-6 col-md-3 col">
             <div className="mb-4">
-              <h3 className="fw-bold mb-9 h4 text-white">Other Link</h3>
+              <h3 className="fw-bold mb-9 h4 text-white">SPORTS</h3>
               <ul className="list-unstyled nav nav-footer flex-column nav-x-0">
-                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Media</a></li>
-                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Influencers</a></li>
-                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Blog</a></li>
-                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">For Startup Hiring</a></li>
-                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Contact Us</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Aquatic</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Swimming</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Diving</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Water Polo</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Archery</a></li>
               </ul>
             </div>
           </div>
           <div className="col-lg-2 col-md-3 col-6 col-md-3 col">
             <div className="mb-4">
-              <h3 className="fw-bold mb-9 h4 text-white">Legal</h3>
+              <h3 className="fw-bold mb-9 h4 text-white">UKGOC Info</h3>
               <ul className="list-unstyled nav nav-footer flex-column nav-x-0">
-                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Privacy Policy</a></li>
-                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Terms and conditions</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Director Sports,</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Sports Directorate, Uttarakhand</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Maharana Pratap Sports College Campus,Raipur, Dehradun</a></li>
+                <li><a href="#" className="nav-link px-0 text-white h5 fw-light">Pin- 248008</a></li>
+             
               </ul>
-              <button type="submit" className="fw-bold h3 px-5 py-5 mt-7 mb-0 btn btn-primary">
-                <img src="../../phone-in-talk.svg" alt="Phone Icon" className="me-2" /> Schedule a call
-              </button>
+             
             </div>
           </div>
         </div>
